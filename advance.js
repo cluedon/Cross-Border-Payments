3645f03640a37bac68647eaefc8290578ccadabc
cluedon <134339245+cluedon@users.noreply.github.com>
2023-06-16 19:57:25 +0100

    Create advance.js

---
+++ advance.js
@@ -0,0 +1,19 @@
+import { ContractKit } from '@celo/contractkit';
+
+// Celo network URL
+const celoNetworkURL = '<your-celo-network-url>';
+
+// Create a new ContractKit instance
+const kit = ContractKit.newKit(celoNetworkURL);
+
+// Connect to the Celo network
+async function connectToCeloNetwork() {
+  try {
+    await kit.connection.connect();
+    console.log('Connected to Celo network');
+  } catch (error) {
+    console.error('Failed to connect to Celo network:', error);
+  }
+}
+
+connectToCeloNetwork();
